5ee24b82302ace8603f0b022d07d147ca475d166
trevorhorst <trevorhorst1212@gmail.com>
2018-03-01 22:38:37 -0500

    baseline server, still needs some work and added functionality. Cleaned up a few things and added some documentation

---
+++ src/default_alt/components/Index.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Dropzone from 'react-dropzone'
+import FlatButton from 'material-ui/FlatButton';
+
+import customBaseTheme from '../customBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+var customMuiTheme = getMuiTheme( customBaseTheme );
+import RaisedButtom from 'material-ui/RaisedButton';
+
+import API from 'api';
+
+class SubComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    onDrop(files) {
+
+        // Actually send the file
+        let comp = this;
+        API.sendFile(files)
+        .then(function(resp) {
+            console.log(resp)
+        })
+        .catch(function(error) {
+            console.log("ERROR", error)
+        })
+    }
+
+
+    onUploadProgress( files ) {
+        console.log( "Prgress.." )
+    }
+
+    render() {
+
+        let dropzoneRef;
+
+        return (
+            <MuiThemeProvider muiTheme={customMuiTheme} >
+                <div>
+                    <Dropzone 
+                        onUploadProgress={this.onUploadProgress.bind(this)}
+                        onDrop={this.onDrop.bind(this)} 
+                        ref={(node) => { dropzoneRef = node; }}  
+                        disableClick
+                    >
+                        <p>Drop your config file here.</p>
+                    </Dropzone>
+                    <FlatButton label="Choose File" onClick={() => { dropzoneRef.open() }}/>
+                </div>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+export default SubComponent;
